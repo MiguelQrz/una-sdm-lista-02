6915bd4d0fba8bb01488874d8d2743fcc539dac7
MiguelQrz <mqroz5@gmail.com>
2026-03-15 13:19:57 -0300

    Create Shopping.cs

---
+++ Shopping.cs
@@ -0,0 +1,8 @@
+public class Shopping
+{
+    public string Nome { get; set; }
+    public string Endereco { get; set; }
+    public int QuantidadeLojas { get; set; }
+    public bool PossuiCinema { get; set; }
+    public string HorarioFuncionamento { get; set; }
+}
