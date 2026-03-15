4e5215b2e3835999abcb14614f2077526d0d0816
MiguelQrz <mqroz5@gmail.com>
2026-03-15 13:17:29 -0300

    Create Ferramentas.cs

---
+++ Ferramentas.cs
@@ -0,0 +1,9 @@
+public class Ferramenta
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Categoria { get; set; }
+    public int QuantidadeEstoque { get; set; }
+    public decimal PrecoUnitario { get; set; }
+    public string Fabricante { get; set; }
+}
