235bf5f0268d7ce2be919fc82cae30ad41ef4e37
MiguelQrz <mqroz5@gmail.com>
2026-03-15 13:19:28 -0300

    Create Universidade.cs

---
+++ Universidade.cs
@@ -0,0 +1,8 @@
+public class Universidade
+{
+    public string Nome { get; set; }
+    public string Cnpj { get; set; }
+    public string Campus { get; set; }
+    public List<string> CursosOferecidos { get; set; }
+    public bool Publica { get; set; }
+}
