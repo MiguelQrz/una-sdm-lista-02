b9d4965335bdab9ef46c378ca3c9ded22afffb02
MiguelQrz <mqroz5@gmail.com>
2026-03-15 13:18:14 -0300

    Create Veiculos.cs

---
+++ Veiculos.cs
@@ -0,0 +1,10 @@
+public class Veiculo
+{
+    public string Vin { get; set; }
+    public string Modelo { get; set; }
+    public string Marca { get; set; }
+    public int AnoFabricacao { get; set; }
+    public string Cor { get; set; }
+    public decimal PrecoVenda { get; set; }
+    public bool Disponivel { get; set; }
+}
