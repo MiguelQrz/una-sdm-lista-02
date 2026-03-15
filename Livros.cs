d3cb0ac1708edb5a092ad7ba88a8bfd673c677cd
MiguelQrz <mqroz5@gmail.com>
2026-03-15 13:18:52 -0300

    Create Livros.cs

---
+++ Livros.cs
@@ -0,0 +1,9 @@
+public class Livro
+{
+    public string Isbn { get; set; }
+    public string Titulo { get; set; }
+    public string Autor { get; set; }
+    public int NumeroPaginas { get; set; }
+    public string Genero { get; set; }
+    public string Editora { get; set; }
+}
